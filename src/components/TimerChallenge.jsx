ab47aa820ebaa9043dc4629800be1ce23c4fef91
gbenro <gbenro_adesoye@yahoo.com>
2024-02-12 11:55:15 -0600

    added time and logic to timer

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,16 +1,34 @@
+import { useState } from "react"
+
+
+
+
 export default function TimerChallenge({title, targetTime}){
+    const [timerStarted, SetTimerStarted]=useState(false);
+const [timerExpired, SetTimerExpired] = useState(false);
+
+function handleStart(){
+    setTimeout(()=>{
+        SetTimerExpired(true)
+    },targetTime * 1000)
+
+    SetTimerStarted(true)
+}
+
+
     return <section className="challenge">  
     <h2>{title}</h2>
+    {timerExpired && <p>You Lost!!!</p>}
     <p className="challenge-time"> 
     {targetTime} second{targetTime >1 ? "s":""}
      </p>
      <p>
-        <button>
-            Start Challenge
+        <button onClick={handleStart}>
+        {timerStarted ?"Stop":"Start"} Challenge
         </button>
      </p>
-     <p className="">
-    Time is running...../ time inactive
+     <p className={timerStarted?"active": undefined}>
+   {timerStarted?" Time is running" : "Timer inactive"}
      </p>
     </section>
 }
